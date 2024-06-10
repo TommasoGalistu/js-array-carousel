f98ff47b4964f511d65e9f600c11a49f85ce741e
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-10 14:50:22 +0200

    aggiunta: array con indirizzi foto

--- js/myjs.js
+++ js/myjs.js
@@ -1,3 +1,12 @@
 // individuare il numero di foto per fagli cambiare dimensione 
 let numFoto = 5;
 let modificaNumFoto = document.documentElement.style.setProperty('--numFoto', numFoto);
+
+// creazione array con indirizzi foto
+let arrFoto = [
+    'img/01.webp',
+    'img/02.webp',
+    'img/03.webp',
+    'img/04.webp',
+    'img/05.webp',
+]
