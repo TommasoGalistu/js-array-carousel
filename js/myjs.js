cad14b5c0c4e0bcfc6662ba4cac415fb28e36da8
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-10 16:05:35 +0200

    modifica: aggiunta ; nel file js

--- js/myjs.js
+++ js/myjs.js
@@ -1,6 +1,6 @@
 // variabili dei contenitori dove inserire le foto
-let contFotoPrincipale = document.querySelector('.contFoto')
-let contFotoPiccole = document.querySelector('.contFotoPiccole')
+let contFotoPrincipale = document.querySelector('.contFoto');
+let contFotoPiccole = document.querySelector('.contFotoPiccole');
 
 // aggiunta bottoni in variabili
 const bottIndietro = document.querySelector('.buttonIndietro');
@@ -14,7 +14,7 @@ let arrFoto = [
     'img/03.webp',
     'img/04.webp',
     'img/05.webp',
-]
+];
 // creazione di array descrizione ALT foto
 let arrDescrizione = [
     'foto spiderman',
@@ -22,7 +22,7 @@ let arrDescrizione = [
     'immagine gioco che non conosco',
     'immagine gioco gatto',
     'immagine avengers',
-]
+];
 
 // individuare il numero di foto per fagli cambiare dimensione 
 let numFoto = 0;
@@ -30,22 +30,22 @@ let numFoto = 0;
 // ciclo per creare immagini da database e inserirle
 for (let i = 0; i < arrFoto.length; i++) {
     // creazione di due nodi
-    let itemFoto = document.createElement('img')
-    let itemFoto2 = document.createElement('img')
+    let itemFoto = document.createElement('img');
+    let itemFoto2 = document.createElement('img');
     // modifica src
-    itemFoto.src = arrFoto[i]
-    itemFoto2.src = itemFoto.src
+    itemFoto.src = arrFoto[i];
+    itemFoto2.src = itemFoto.src;
     // modifica alt
-    itemFoto.alt = arrDescrizione[i]
-    itemFoto2.alt = itemFoto.alt
+    itemFoto.alt = arrDescrizione[i];
+    itemFoto2.alt = itemFoto.alt;
     // aggiunta della classe per gestire l'apparizione
-    itemFoto.classList.add('fotoPrincipale')
-    itemFoto2.classList.add('fotoPrincipale2')
+    itemFoto.classList.add('fotoPrincipale');
+    itemFoto2.classList.add('fotoPrincipale2');
     // appendo i due nodi ai container
-    contFotoPrincipale.append(itemFoto)
-    contFotoPiccole.append(itemFoto2)
+    contFotoPrincipale.append(itemFoto);
+    contFotoPiccole.append(itemFoto2);
     // contatore foto così da gestire le dimensioni, guarda sotto il for
-    numFoto++
+    numFoto++;
 }
 
 // creazione dinamica della dimensione foto laterale
@@ -55,8 +55,8 @@ let modificaNumFoto = document.documentElement.style.setProperty('--numFoto', nu
 let items = document.getElementsByClassName('fotoPrincipale');
 let items2 = document.getElementsByClassName('fotoPrincipale2');
 let contatore = 0;
-items[contatore].classList.add('active')
-items2[contatore].classList.add('active')
+items[contatore].classList.add('active');
+items2[contatore].classList.add('active');
 
 
 
