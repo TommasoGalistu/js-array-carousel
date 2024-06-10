bb512b9d83261bc89c521e07abe6192106c31ea6
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-10 15:04:56 +0200

    aggiunta: arr descrizione e ciclo per creare foto principale

--- js/myjs.js
+++ js/myjs.js
@@ -1,10 +1,15 @@
 // individuare il numero di foto per fagli cambiare dimensione 
-let numFoto = 5;
+let numFoto = 0;
 let modificaNumFoto = document.documentElement.style.setProperty('--numFoto', numFoto);
+
+// variabili dei contenitori dove inserire le foto
+let contFotoPrincipale = document.querySelector('.contFoto')
+let contFotoPiccole = document.querySelector('.contFotoPiccole')
 
 // aggiunta bottoni in variabili
 const bottIndietro = document.querySelector('.buttonIndietro')
 const bottAvanti = document.querySelector('.buttonAvanti')
+
 
 // creazione array con indirizzi foto
 let arrFoto = [
@@ -14,7 +19,21 @@ let arrFoto = [
     'img/04.webp',
     'img/05.webp',
 ]
+// creazione di array descrizione ALT foto
+let arrDescrizione = [
+    'foto spiderman',
+    'immagine gioco zelda',
+    'immagine gioco che non conosco',
+    'immagine gioco gatto',
+    'immagine avengers',
+]
 
+for (let i = 0; i < arrFoto.length; i++) {
+    let itemFoto = document.createElement('img')
+    itemFoto.src = arrFoto[i]
+    itemFoto.alt = arrDescrizione[i]
+    contFotoPrincipale.append(itemFoto)
+}
 // creazione evento per andare avanti
 bottAvanti.addEventListener('click', () => {
     console.log('dentro')
